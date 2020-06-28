d5453e8fdcf1279b478fceb57b83a89fe88ee54d
Brandon Papin <42751309+bpapin19@users.noreply.github.com>
2020-06-27 18:35:00 -0700

    Add files via upload

---
+++ RestaurantTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Brandon Papin on 6/17/20.
+//  Copyright © 2020 Brandon Papin. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = 30
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
